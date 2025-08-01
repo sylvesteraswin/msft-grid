0b133aeee71f9b3ca84b96ede84ab2ecb18fcd7f
Sylvester Aswin <me@sylvesteraswin.com>
2025-08-01 14:03:12 +0200

    refactor: optimize visible items sorting in Home component using useMemo

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,23 @@
 "use client";
-import { useState } from "react";
+import { useState, useMemo } from "react";
 
 import { Posters } from "@/components/posters";
 import { Thumbnails } from "@/components/thumbnails";
 
 export default function Home() {
   const [visible, setVisible] = useState(new Set<number>());
+
+  const sortedVisible = useMemo(
+    () => [...visible].sort((a, b) => a - b),
+    [visible]
+  );
+
   return (
     <div>
       <h1 className="font-black text-3xl">Our Layout</h1>
       <main className="flex gap-2 w-full">
         <section className="flex-[80%] grow-1">
-          <Posters visibleItems={[...visible].sort((a, b) => a - b)} />
+          <Posters visibleItems={sortedVisible} />
         </section>
         <aside className="flex-[20%]">
           <Thumbnails visible={visible} setVisible={setVisible} />
